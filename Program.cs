5e2a26065c4735c6d1904f19224d070189a5391d
mjesposito25 <matt.joseph.esposito@gmail.com>
2022-07-14 21:25:05 -0400

    Project Creation

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using SpotifyAPI.Web;
+
+class Program
+{
+    public static async Task Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("keys.txt");
+
+            var ID = sr.ReadLine();
+            var SECRET = sr.ReadLine();
+
+            sr.Close(); 
+
+            var config = SpotifyClientConfig.CreateDefault();
+
+            var request = new ClientCredentialsRequest(ID, SECRET);
+            var response = await new OAuthClient(config).RequestToken(request);
+
+            var spotify = new SpotifyClient(config.WithToken(response.AccessToken));
+
+            var track = await spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+            Console.WriteLine(track.Name);
+        }
+}
